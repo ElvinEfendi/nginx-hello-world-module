78c876f48f890c169749a5e282374add68fd804d
Elvin Efendi <elvin.efendiyev@gmail.com>
2017-04-09 16:34:48 -0400

    initial baps definition

---
+++ baps.rb
@@ -0,0 +1,10 @@
+deb do
+  name 'nginx-hello-world-module to load dynamically'
+  version '0.1'
+  dependencies []
+  description 'The package produces .so file for the nginx-hello-world-module module compiled with the given version of Nginx'
+
+  build do
+    # TODO
+  end
+end
